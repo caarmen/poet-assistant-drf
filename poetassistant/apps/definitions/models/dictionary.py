6f1b496df362b80b4f74002a514aa5c9a6e8ad59
Carmen Alvarez <c@rmen.ca>
2022-02-27 14:43:48 +0100

    Add definitions endpoint
    * Configure pagination in the project settings
    * Add the embedded database in the db module
    * Add a `Dictionary` model for the `dictionary` table in the DB.
    * Add a `DictionarySerializer` to convert the dictionary entries to json
      - Use `part-of-speech` in the json, even though the db field is `part_of_speech`
    * Add a `DefinitionSet` using a `SearchFilter`, allowing exact searches on the `word`.
    * Add a router/urls pointing ot the `DefinitionSet`

---
+++ poetassistant/apps/definitions/models/dictionary.py
@@ -0,0 +1,33 @@
+# Copyright (c) 2022 - present Carmen Alvarez
+#
+# This file is part of Poet Assistant.
+#
+# Poet Assistant is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Poet Assistant is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Poet Assistant.  If not, see <http://www.gnu.org/licenses/>.
+from django.db import models
+
+from poetassistant.apps.definitions import apps
+
+
+class Dictionary(models.Model):
+    rowid = models.IntegerField(primary_key=True)
+    word = models.CharField(max_length=128)
+    part_of_speech = models.CharField(max_length=1)
+    definition = models.TextField()
+
+    objects = models.Manager()
+
+    class Meta:
+        db_table = 'dictionary'
+        unique_together = ("word", "definition")
+        app_label = apps.DefinitionsConfig.name
---
+++ poetassistant/apps/definitions/serializers.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2022 - present Carmen Alvarez
+#
+# This file is part of Poet Assistant.
+#
+# Poet Assistant is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Poet Assistant is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Poet Assistant.  If not, see <http://www.gnu.org/licenses/>.
+from rest_framework import serializers
+
+from poetassistant.apps.definitions.models.dictionary import Dictionary
+
+
+class DictionarySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Dictionary
+        fields = ['part-of-speech', 'word', 'definition']
+        extra_kwargs = {
+            'part-of-speech': {'source': 'part_of_speech'}
+        }
