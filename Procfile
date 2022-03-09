da5690119ba0331ca0eb91de168f79f2e08bc970
Carmen Alvarez <c@rmen.ca>
2022-03-09 20:54:22 +0100

    Set up heroku configuration
    https://devcenter.heroku.com/articles/django-app-configuration

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn poetassistant.wsgi
--- poetassistant/settings.py
+++ poetassistant/settings.py
@@ -25,6 +25,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import django_heroku
 import os
 from pathlib import Path
 
@@ -148,3 +149,5 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
+
+django_heroku.settings(locals())
