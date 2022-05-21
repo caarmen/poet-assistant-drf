c08c2254da1b090a40ac6a09ff6a86ced97d07c1
Carmen Alvarez <c@rmen.ca>
2022-05-21 22:25:08 +0200

    Use the pylint-json2checkstyle hosted on pypi

--- scripts/codecheck.bash
+++ scripts/codecheck.bash
@@ -1,5 +1,4 @@
 mkdir -p reports
-pip install --index-url https://test.pypi.org/simple/ pylint_json2checkstyle-caarmen
 pylint --load-plugins=pylint_json2checkstyle.checkstyle_reporter,pylint_json2html  --output-format=jsonextended:reports/pylint.json,checkstyle poetassistant > reports/pylint.xml
 has_lint_errors=$?
 pylint-json2html -f jsonextended -o reports/pylint.html reports/pylint.json
