99f0a56b2efc030432f3ae023855ad5d70659fe8
Carmen Alvarez <c@rmen.ca>
2022-10-15 13:40:50 +0200

    Use drf-spectacular to generate docs

--- generate_docs.bash
+++ generate_docs.bash
@@ -1,2 +1,1 @@
-python manage.py generateschema --title "Poet Assistant REST Api" --description "English-language tools for writing poetry"  --file docs/openapi-schema.yml
-openapi-generator generate -i docs/openapi-schema.yml -g html2 -o docs
+python ./manage.py spectacular --format openapi-json  > docs/openapi.json
--- poetassistant/settings.py
+++ poetassistant/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django_filters",
     "rest_framework",
+    "drf_spectacular",
     "poetassistant.apps.commonapi.apps.CommonApiConfig",
     "poetassistant.apps.db.apps.DbConfig",
     "poetassistant.apps.definitions.apps.DefinitionsConfig",
@@ -148,5 +149,12 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Poet Assistant API",
+    "DESCRIPTION": "English-language tools for writing poetry",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+}
