6af51e2d8c11fabb13a625b0f166747322447a9b
Carmen Alvarez <c@rmen.ca>
2022-10-15 14:56:36 +0200

    Format code with black

--- poetassistant/urls.py
+++ poetassistant/urls.py
@@ -45,7 +45,11 @@ router.register(r"definitions", DefinitionSet, "definitions")
 router.register(r"wotd", WotdSet, "wotd")
 
 urlpatterns = [
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/schema/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ]
 urlpatterns += router.urls
